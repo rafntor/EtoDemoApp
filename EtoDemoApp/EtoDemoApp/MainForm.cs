bc4e8922114e41b13bfbb74afa9cc8ae8189091c
Tore Fuglestad <tore@fuglestad.no>
2022-01-26 17:37:08 +0100

    listbox demo

--- EtoDemoApp/EtoDemoApp/MainForm.cs
+++ EtoDemoApp/EtoDemoApp/MainForm.cs
@@ -9,49 +9,18 @@ namespace EtoDemoApp
 		public MainForm()
 		{
 			Title = "My Eto Form";
-			MinimumSize = new Size(200, 200);
+			Size = new Size(3, 2) * 200;
 
-			Content = new StackLayout
+			var listbox = new ListBox();
+			for (int i=0; i<40; ++i)
 			{
-				Padding = 10,
-				Items =
-				{
-					"Hello World!",
-					// add more controls here
-				}
-			};
+				listbox.Items.Add($"item with longish text {i}");
+			}
 
-			// create a few commands that can be used for the menu and toolbar
-			var clickMe = new Command { MenuText = "Click Me!", ToolBarText = "Click Me!" };
-			clickMe.Executed += (sender, e) => MessageBox.Show(this, "I was clicked!");
+			var layout = new DynamicLayout();
+			layout.AddRow(listbox, new Panel());
 
-			var quitCommand = new Command { MenuText = "Quit", Shortcut = Application.Instance.CommonModifier | Keys.Q };
-			quitCommand.Executed += (sender, e) => Application.Instance.Quit();
-
-			var aboutCommand = new Command { MenuText = "About..." };
-			aboutCommand.Executed += (sender, e) => new AboutDialog().ShowDialog(this);
-
-			// create menu
-			Menu = new MenuBar
-			{
-				Items =
-				{
-					// File submenu
-					new SubMenuItem { Text = "&File", Items = { clickMe } },
-					// new SubMenuItem { Text = "&Edit", Items = { /* commands/items */ } },
-					// new SubMenuItem { Text = "&View", Items = { /* commands/items */ } },
-				},
-				ApplicationItems =
-				{
-					// application (OS X) or file menu (others)
-					new ButtonMenuItem { Text = "&Preferences..." },
-				},
-				QuitItem = quitCommand,
-				AboutItem = aboutCommand
-			};
-
-			// create toolbar			
-			ToolBar = new ToolBar { Items = { clickMe } };
+			Content = layout;
 		}
 	}
 }
